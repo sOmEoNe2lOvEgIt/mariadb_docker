a633dc23e564f4f642138cdb53ca5f1aa7d0e245
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-07-11 16:15:25 +0200

    Upgraded run script.

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+echo -e "\033[0;32m###########_Starting database container_###########\033[0;0m"
 docker run --detach --network slurm-network --name slurm_mariadb -v /home/mariadb_docker/database:/var/lib/mysql \
 --env MARIADB_USER=slurm-user --env MARIADB_PASSWORD=slurm --env MARIADB_ROOT_PASSWORD=suslurm mariadb
